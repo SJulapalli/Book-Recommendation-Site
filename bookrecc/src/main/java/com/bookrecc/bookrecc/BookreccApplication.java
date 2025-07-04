b516bb7847704e99a26b3ead5b19e12afba91a37
Suhas Julapalli <suhasjulapalli@gmail.com>
2025-07-04 17:32:44 -0400

    initial spring backend test setup

---
+++ bookrecc/src/main/java/com/bookrecc/bookrecc/BookreccApplication.java
@@ -0,0 +1,13 @@
+package com.bookrecc.bookrecc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreccApplication.class, args);
+	}
+
+}
